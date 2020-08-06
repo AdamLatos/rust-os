6c97440167edc829bf7b8985c99aa7c9c0aaef0a
Adam Latos <adam.latos@nokia.com>
2020-08-06 17:37:30 +0200

    initial

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+
+extern crate rlibc;
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop{}
+}
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+    
+    loop{}
+}
